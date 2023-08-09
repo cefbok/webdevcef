c00f17a255c95ee106c2dacf10c9c158da7bfc0e
cefbok <xcef.1991@gmail.com>
2023-08-09 13:50:44 +0800

    assets

--- src/data/data.js
+++ src/data/data.js
@@ -1,19 +1,19 @@
 const data = [
    {
      title: 'Tool Keeper System (Capstone)',
-     imgUrl: 'src/assets/tks2.png',
+     imgUrl: '/assets/tks2.png',
      stack: ['HTML', 'CSS','JavaScript', 'Django', 'MySQL'],
      link: 'https://tkstupc.com/',
    },
    {
      title: "Cef's Portfolio",
-     imgUrl: 'src/assets/port.png',
+     imgUrl: '/assets/port.png',
      stack: ['React', 'Tailwind', 'Vite'],
      link: 'https://github.com/cefbok/webdevcef',
    },
    {
      title: 'Get Form',
-     imgUrl: 'src/assets/3.jpg',
+     imgUrl: '/assets/3.jpg',
      stack: ['HTML', 'CSS', 'JavaScript'],
      link: '',
    },
